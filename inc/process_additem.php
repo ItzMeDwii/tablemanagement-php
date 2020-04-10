ef4da6e1b843afb3af4c23b21b8f9252a2c409e4
Satya Bagus Dwiatmaja <dwii5359@azalelnation.com>
2020-04-11 06:15:34 +0700

    Update process_additem.php

--- inc/process_additem.php
+++ inc/process_additem.php
@@ -1,3 +1,36 @@
 <?php
-
+require_once './config.php';
+$config = new Config();
 ?>
+
+<html>
+<head>
+   <meta http-equiv="refresh" content="2; url=<?php echo $config->index; ?>">
+</head>
+
+<?php
+$mysqli = new mysqli($config->db_host, $config->db_username, $config->db_password, $config->db_name);
+
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$kode = $_POST['kodebarang'];
+$nama = $_POST['namabarang'];
+$harga = $_POST['harga'];
+$jumlah = $_POST['jumlah'];
+
+$addprocess = $mysqli->query("INSERT INTO storeman_items (code,name,price,stock) VALUES ('$kode', '$nama', '$harga', '$jumlah')");
+
+if (!$addprocess) {
+	echo '<p class="text-center">Error!</p>';
+}
+if ($addprocess) {
+	echo '<p class="text-center">Done!</p>';
+}
+
+$mysqli->close();
+?>
+
+</html>
