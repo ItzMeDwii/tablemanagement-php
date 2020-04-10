86468083fb0ed99e43822cfbdf9c80509a82eca0
Satya Bagus Dwiatmaja <dwii5359@azalelnation.com>
2020-04-11 06:09:29 +0700

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1,3 +1,23 @@
 <?php
 
+final class Config {
+    public static $TRUE = "1", $FALSE = "0";
+
+    public function __construct($connect = true) {
+        
+    // Website Info
+    $this->name = "TableMan";
+    $this->index = "index.php";
+    
+    // Database Info
+    $this->db_driver = "mysqli";
+    $this->db_host = "localhost";
+    $this->db_port = "3306";
+    $this->db_username = "";
+    $this->db_password= "";
+    $this->db_name= "n";
+        
+}
+}
+
 ?>
