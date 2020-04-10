c4515c259ae76aa1c635c438d09caf4c514aeddd
Satya Bagus Dwiatmaja <dwii5359@azalelnation.com>
2020-04-11 06:15:55 +0700

    Update process_deleteitem.php

--- inc/process_deleteitem.php
+++ inc/process_deleteitem.php
@@ -1,3 +1,37 @@
 <?php
-
+require_once './config.php';
+$config = new Config();
 ?>
+
+<html>
+<head>
+   <meta http-equiv="refresh" content="2; url=<?php echo $config->index; ?>">
+</head>
+<?php
+
+$mysqli = new mysqli($config->db_host, $config->db_username, $config->db_password, $config->db_name);
+
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$kode = $_GET['id'];
+
+if (!kode){
+	echo "INVALID CODE!";
+}
+if ($kode) {
+$addprocess = $mysqli->query("DELETE FROM storeman_items WHERE code='". $kode ."';");
+
+if (!$addprocess) {
+	echo '<p class="text-center">Error!</p>';
+}
+if ($addprocess) {
+	echo '<p class="text-center">Done!</p>';
+}
+}
+$mysqli->close();
+?>
+
+</html>
