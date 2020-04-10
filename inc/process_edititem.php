35b523cbeeff468e2ab139ca9d3a8f82947cf156
Satya Bagus Dwiatmaja <dwii5359@azalelnation.com>
2020-04-11 06:16:14 +0700

    Update process_edititem.php

--- inc/process_edititem.php
+++ inc/process_edititem.php
@@ -1,3 +1,36 @@
 <?php
-
+require_once './config.php';
+$config = new Config();
 ?>
+
+<html>
+<head>
+   <meta http-equiv="refresh" content="2; url=<?php echo $config->index; ?>">
+</head>
+<?php
+
+$mysqli = new mysqli($config->db_host, $config->db_username, $config->db_password, $config->db_name);
+
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$kode = $_POST['kodebarang'];
+$nama = $_POST['namabarang'];
+$harga = $_POST['harga'];
+$jumlah = $_POST['jumlah'];
+
+$addprocess = $mysqli->query("UPDATE storeman_items SET name='". $nama ."', price='". $harga ."', stock='". $jumlah ."' WHERE code='". $kode ."';");
+
+if (!$addprocess) {
+	echo '<p class="text-center">Error!</p>';
+}
+if ($addprocess) {
+	echo '<p class="text-center">Done!</p>';
+}
+
+$mysqli->close();
+?>
+
+</html>
